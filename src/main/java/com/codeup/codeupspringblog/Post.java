90b2b8127bd340c2d41695b14ccda4c876191e86
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-03-27 09:38:45 -0500

    working on index and show for posts

---
+++ src/main/java/com/codeup/codeupspringblog/Post.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupspringblog;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    String title;
+    String body;
+}
--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,9 +1,13 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.Post;
 import org.apache.coyote.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 class PostController {
@@ -46,5 +50,19 @@ class PostController {
         }
         model.addAttribute("response", "You guessed: " + number + "\nThe correct answer was " + randomDiceRoll + "\n" + response);
         return "result";
+    }
+    @GetMapping("/index-posts")
+    public String indexPost(Model model, @PathVariable long postId){
+//        create new post using just id
+        model.addAttribute("post", new Post());
+        return "index";
+    }
+    @GetMapping("/show-post")
+    public String showPosts(Model model){
+//        create list of posts
+        List<Post> postList = new ArrayList<>();
+
+        model.addAttribute("post", postList);
+        return "show";
     }
 }
