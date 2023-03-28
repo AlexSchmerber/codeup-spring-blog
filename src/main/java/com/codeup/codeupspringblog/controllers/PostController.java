4c57dd642fc1b4ab7004e81eda306ac9fab6d0b4
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-03-28 12:18:20 -0500

    post create.html exercise

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -18,47 +18,28 @@ class PostController {
     private final PostRepository postDao;
 
 
-    @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String postPage() {
-        return "Post Index Page";
-    }
+//    @GetMapping("/posts")
+//    public String postPage() {
+//        return "/posts/show";
+//    }
+//
+//    @GetMapping("/posts{id}")
+//    public String postPageWithId(@PathVariable int id) {
+//        return "Post With Id: " + id;
+//    }
 
-    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String postPageWithId(@PathVariable int id) {
-        return "Post With Id: " + id;
-    }
-
-    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @GetMapping("/posts/create")
     public String getCreatePost(Model model) {
         model.addAttribute("post", new Post());
         return "/posts/create";
     }
 
-    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-    public String postCreatePost(@ModelAttribute Post post, Model model) {
-//        model.addAttribute("post", post);
+    @PostMapping("/posts/create")
+    public String postCreatePost(@ModelAttribute Post post) {
         postDao.save(post);
         return "redirect:/show-post";
     }
 
-    @GetMapping("/roll-dice")
-    public String rollDice(){
-        return "dice";
-    }
-    @GetMapping("/roll-dice/{number}")
-    public String rollDice(@PathVariable int number, Model model){
-        int randomDiceRoll = (int) Math.floor(Math.random() * (6 - 1) + 1);
-        String response;
-        if(number == randomDiceRoll){
-            response = "GOOD GUESS!";
-        } else {
-            response = "bad guess :(";
-        }
-        model.addAttribute("response", "You guessed: " + number + "\nThe correct answer was " + randomDiceRoll + "\n" + response);
-        return "result";
-    }
     @GetMapping("/index-posts/{postId}")
     public String indexPost(Model model, @PathVariable long postId){
 //        create new post using just id
@@ -71,3 +52,20 @@ class PostController {
         return "/posts/show";
     }
 }
+
+//    @GetMapping("/roll-dice")
+//    public String rollDice(){
+//        return "dice";
+//    }
+//    @GetMapping("/roll-dice/{number}")
+//    public String rollDice(@PathVariable int number, Model model){
+//        int randomDiceRoll = (int) Math.floor(Math.random() * (6 - 1) + 1);
+//        String response;
+//        if(number == randomDiceRoll){
+//            response = "GOOD GUESS!";
+//        } else {
+//            response = "bad guess :(";
+//        }
+//        model.addAttribute("response", "You guessed: " + number + "\nThe correct answer was " + randomDiceRoll + "\n" + response);
+//        return "result";
+//    }
